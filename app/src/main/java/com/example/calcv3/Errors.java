f8c8fd0082395f41484b6d1a1e8c15318784cd43
maxim <estonmax@gmail.com>
2021-05-30 15:36:14 +0300

    1. Added functional for buttons 2. Added other errors for wrong results, which can happen 3. Added landscape activity and new calculator's keyboard 4. Added calculation

---
+++ app/src/main/java/com/example/calcv3/Errors.java
@@ -0,0 +1,16 @@
+package com.example.calcv3;
+
+public class Errors {
+
+    public String getTwoSymbols() {
+        return "ERROR: 2 symbols";
+    }
+
+    public String getTooMuchPointsOnNumber() {
+        return "ERROR: more than 1 point";
+    }
+
+    public String getFirstSymbol() {
+        return "ERROR: first sign - not number";
+    }
+}
